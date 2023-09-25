830d42d42e4908a4ca931aeff734c49e94108cf5
OutOfBounds <ghjreto@gmail.com>
2023-09-25 03:55:10 +0300

    task3

---
+++ SQLQuery231.sql
@@ -0,0 +1,5 @@
+SELECT PATINDEX('%[0-9]%',data.string)
+FROM data
+WHERE ISNUMERIC(SUBSTRING(data.string, PATINDEX('%[0-9]%',data.string), CHARINDEX(' ', CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' '))-PATINDEX('%[0-9]%',CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' ')))) = 1
+	AND RIGHT(data.string, LEN(SUBSTRING(data.string, PATINDEX('%[0-9]%',data.string), CHARINDEX(' ', CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' '))-PATINDEX('%[0-9]%',CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' ')))))
+	<> SUBSTRING(data.string, PATINDEX('%[0-9]%',data.string), CHARINDEX(' ', CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' '))-PATINDEX('%[0-9]%',CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' ')))
