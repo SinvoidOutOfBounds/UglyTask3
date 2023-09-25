b3a0cbb673c6cb658d00dddccef4043643d90c7f
SinvoidOutOfBounds <131470210+SinvoidOutOfBounds@users.noreply.github.com>
2023-09-25 04:01:20 +0300

    Update SQLQuery231.sql

--- SQLQuery231.sql
+++ SQLQuery231.sql
@@ -3,3 +3,6 @@ FROM data
 WHERE ISNUMERIC(SUBSTRING(data.string, PATINDEX('%[0-9]%',data.string), CHARINDEX(' ', CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' '))-PATINDEX('%[0-9]%',CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' ')))) = 1
 	AND RIGHT(data.string, LEN(SUBSTRING(data.string, PATINDEX('%[0-9]%',data.string), CHARINDEX(' ', CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' '))-PATINDEX('%[0-9]%',CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' ')))))
 	<> SUBSTRING(data.string, PATINDEX('%[0-9]%',data.string), CHARINDEX(' ', CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' '))-PATINDEX('%[0-9]%',CONCAT(SUBSTRING (data.string, PATINDEX('%[0-9]%',data.string), LEN(data.string) - PATINDEX('%[0-9]%',data.string)+1),' ')))
+-- Отсутствие знания о том, где заканчивается число сильно увеличивает проверяемую на условия подстроку
+-- ISNUMERIC проверяет, что после числа не идёт буква
+-- RIGHT <> В этом условии проверяется, что число не находится в конце строки.
